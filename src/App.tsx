d6dfcd978aa173c0fd18c0de1044677a57edcae1
kramire <kramire@users.noreply.github.com>
2020-10-10 13:13:28 +0200

    feat: responsive app nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import { AppWrapper } from "./components";
+import { AppWrapper, AppNavBar } from "./components";
 
 function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <AppWrapper>App Here</AppWrapper>
+        <AppWrapper>
+          <AppNavBar />
+        </AppWrapper>
       </ThemeProvider>
     </Router>
   );
---
+++ src/components/AppNavBar.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { capitalize } from "../lib/utils";
+import { Icon } from "semantic-ui-react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: auto;
+  padding: 2em;
+
+  * {
+    color: ${props => props.theme.colors.primary};
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  @media (max-width: ${props => props.theme.screenSize.tablet}) {
+    z-index: 1;
+    padding: 1em;
+  }
+`;
+
+const MenuWrapper = styled.div<{ isMenuOpen: boolean }>`
+  /* then, arrange our links top to bottom */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  color: ${props => props.theme.colors.secondary} > a {
+    margin-left: 1em;
+  }
+
+  @media (max-width: ${props => props.theme.screenSize.tablet}) {
+    z-index: 0;
+
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+
+    /* first, make our dropdown cover the screen */
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 100vh;
+    /* fix nav height on mobile safari, where 100vh is a little off */
+    height: -webkit-fill-available;
+
+    margin: 0;
+    padding-left: 7vw;
+    padding-right: 7vw;
+
+    opacity: ${props => (props.isMenuOpen ? "1" : "0")}; /* fade out */
+    transform: ${props =>
+      props.isMenuOpen ? "translateY(0)" : "translateY(-100%)"}; /* fade out */
+    transition: transform 0.2s, opacity 0.2s;
+  }
+`;
+
+const MenuIcon = styled(Icon)`
+  &&&&&& {
+    display: none;
+
+    @media (max-width: ${props => props.theme.screenSize.tablet}) {
+      z-index: 1;
+      display: initial;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+const MenuOption = styled.div`
+  padding: 0 1em;
+
+  @media (max-width: ${props => props.theme.screenSize.tablet}) {
+    padding: 1em 0;
+    font-size: 20px;
+  }
+`;
+
+export const AppNavBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const history = useHistory();
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  const menuOptions = ["log", "analytics", "settings"];
+
+  return (
+    <Wrapper>
+      <div onClick={() => history.push("/")}>Running Log</div>
+      {isMenuOpen ? (
+        <MenuIcon name="close" onClick={toggleMenu} />
+      ) : (
+        <MenuIcon name="align justify" onClick={toggleMenu} />
+      )}
+      <MenuWrapper isMenuOpen={isMenuOpen}>
+        {menuOptions.map((option, idx) => (
+          <MenuOption key={idx} onClick={() => history.push(`/${option}`)}>
+            {capitalize(option)}
+          </MenuOption>
+        ))}
+      </MenuWrapper>
+    </Wrapper>
+  );
+};
